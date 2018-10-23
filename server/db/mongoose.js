9a16789e5783d30e4b2b7f4d98b973e25861734b
rekatDev <sven_grunwald@gmx.de>
2018-10-23 11:50:37 +0200

    Add PATCH /todos/:id

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const url = process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp";
 mongoose.Promise = global.Promise;
+mongoose.set('useFindAndModify', false);
 mongoose.connect(
   url,
   { useNewUrlParser: true }
